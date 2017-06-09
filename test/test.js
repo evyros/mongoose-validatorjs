4fca2e7dd7a1fde10a240ddda22352c7b4ecad31
Evyatar Rosner <evyatar1919@gmail.com>
2017-06-09 19:37:44 +0300

    eslint and mocha + chai integrated

---
+++ test/test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+
+describe('Array', function() {
+	describe('#indexOf()', function() {
+		it('should return -1 when the value is not present', function() {
+			expect({ab: 1}).to.not.have.property('a');
+			expect([1, 2]).to.be.an('array').that.does.not.include(3);
+		});
+	});
+});
