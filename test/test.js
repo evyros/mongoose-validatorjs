0dea3299aef2589f6a3a91bde4fae636c4153a01
Evyatar Rosner <evyatar1919@gmail.com>
2017-06-09 20:53:46 +0300

    integrate mongoose into project

--- test/test.js
+++ test/test.js
@@ -1,11 +1,16 @@
-import { expect } from 'chai';
+import expect from 'chai';
+import mongoose from 'mongoose';
+mongoose.Promise = Promise;
 
 
 describe('Array', function() {
+
 	describe('#indexOf()', function() {
+
 		it('should return -1 when the value is not present', function() {
 			expect({ab: 1}).to.not.have.property('a');
 			expect([1, 2]).to.be.an('array').that.does.not.include(3);
 		});
 	});
+
 });
