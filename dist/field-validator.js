604d47d88392e434f06de70a9c8362459963e8ec
Evyatar Rosner <evyatar1919@gmail.com>
2017-06-11 22:46:42 +0300

    build project

---
+++ dist/field-validator.js
@@ -0,0 +1,96 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+exports.FieldValidator = undefined;
+
+var _validator = require('validator');
+
+var _validator2 = _interopRequireDefault(_validator);
+
+var _defaultErrors = require('./default-errors');
+
+var _defaultErrors2 = _interopRequireDefault(_defaultErrors);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+class FieldValidator {
+
+	constructor(schema, field) {
+		this._schema = schema;
+		this._field = field;
+	}
+
+	getSchema() {
+		return this._schema;
+	}
+
+	getField() {
+		return this._field;
+	}
+
+	createPath() {
+		return this.getSchema().path(this.getField());
+	}
+
+	addValidator(name, args, message) {
+		if (name in this) {
+			return this[name](message);
+		}
+		if (name in _validator2.default) {
+			return this.useValidatorJS(name, args, message);
+		}
+		throw new Error('Validator `' + name + '` does not exist');
+	}
+
+	useValidatorJS(name, args, message) {
+		this.buildValidator({
+			validator: function (value) {
+				if (isEmpty(value)) {
+					return true;
+				}
+				return _validator2.default[name](value, args);
+			},
+			message: message || _defaultErrors2.default[name]
+		}, args);
+	}
+
+	required(message) {
+		this.buildValidator({
+			validator: function (value) {
+				return !isEmpty(value);
+			},
+			message: message || this.getField() + ' is required'
+		});
+	}
+
+	buildValidator(obj, args) {
+		this.createPath().validate({
+			isAsync: false,
+			validator: obj.validator,
+			message: interpolateMessage(obj.message, args)
+		});
+	}
+
+}
+
+exports.FieldValidator = FieldValidator;
+function interpolateMessage(message, args) {
+	if (args === null || typeof args === 'undefined') {
+		args = '';
+	}
+	if (typeof args === 'string') {
+		args = [args];
+	} else if (typeof args === 'object') {
+		args = Object.values(args);
+	}
+	return message.replace(/{ARGS\[(\d+)\]}/g, (replace, argIndex) => {
+		let val = args[argIndex];
+		return typeof val !== 'undefined' ? val : '';
+	});
+}
+
+function isEmpty(value) {
+	return value === '' || value === null || typeof value === 'undefined';
+}
---
+++ dist/mongoose-validatorjs.js
@@ -0,0 +1,50 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+exports.MongooseValidatorjs = undefined;
+
+var _fieldValidator = require('./field-validator');
+
+class ValidatorFactory {
+
+	constructor(schema, field) {
+		const instance = new _fieldValidator.FieldValidator(schema, field);
+		return new Proxy(this, {
+			get: function (className, validator) {
+				return (arg1, arg2) => {
+					let options, message;
+					if (arg1 && arg1.message) {
+						message = arg1.message;
+					} else if (arg2 && arg2.message) {
+						options = arg1;
+						message = arg2.message;
+					} else {
+						options = arg1;
+					}
+					instance.addValidator(validator, options, message);
+					return new ValidatorFactory(schema, field);
+				};
+			}
+		});
+	}
+
+}
+
+class MongooseValidatorjs {
+
+	constructor(schema) {
+		this._schema = schema;
+	}
+
+	getSchema() {
+		return this._schema;
+	}
+
+	field(fieldName) {
+		return new ValidatorFactory(this.getSchema(), fieldName);
+	}
+
+}
+exports.MongooseValidatorjs = MongooseValidatorjs;
