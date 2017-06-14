a6ab0c26bd86740ef868e250ab74218de3325d86
Evyatar Rosner <evyatar1919@gmail.com>
2017-06-14 15:24:50 +0300

    refactor proxy handler in ValidatorFactory

--- src/validator-factory.js
+++ src/validator-factory.js
@@ -3,25 +3,31 @@ import FieldValidator from './field-validator';
 export default class ValidatorFactory {
 
 	constructor(schema, field) {
-		const instance = new FieldValidator(schema, field);
+		function handler(className, validator) {
+			return (arg1, arg2) => {
+				const instance = new FieldValidator(schema, field);
+
+				let options, message;
+				if(arg1 && arg1.message) {
+					// validator with custom message
+					message = arg1.message;
+				}
+				else if(arg2 && arg2.message) {
+					// validator with argument & custom message
+					options = arg1;
+					message = arg2.message;
+				}
+				else {
+					// validator with argument
+					options = arg1;
+				}
+				instance.addValidator(validator, options, message);
+				return new ValidatorFactory(schema, field);
+			};
+		}
+
 		return new Proxy(this, {
-			get: function(className, validator) {
-				return (arg1, arg2) => {
-					let options, message;
-					if(arg1 && arg1.message) {
-						message = arg1.message;
-					}
-					else if(arg2 && arg2.message) {
-						options = arg1;
-						message = arg2.message;
-					}
-					else {
-						options = arg1;
-					}
-					instance.addValidator(validator, options, message);
-					return new ValidatorFactory(schema, field);
-				};
-			}
+			get: handler
 		});
 	}
 
